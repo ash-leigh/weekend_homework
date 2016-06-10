b7cb95fbcfedf31756ddc943cb3c4f9ca09fb15a
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-06-10 13:59:08 +0100

    initial commit

---
+++ pet_shop.rb
@@ -0,0 +1,63 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  pet_shop[:admin][:total_cash] = pet_shop[:admin][:total_cash] + amount
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount_of_pets_sold)
+  pet_shop[:admin][:pets_sold] = pet_shop[:admin][:pets_sold] + amount_of_pets_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed (pet_shop, breed_name)
+  pet_count =[]
+  for pet in pet_shop[:pets]
+    pet_count << pet if pet[:breed] == breed_name
+  end
+  return pet_count
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    return pet if pet[:name] == pet_name
+  end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    pet.delete(:name) if pet[:name] == pet_name
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  return pet_shop[:pets] + new_pet.to_i
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
